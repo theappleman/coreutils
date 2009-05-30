f35459603e74c07c1580fc75bca69003a825cbec
Daniel Cordero <theappleman@gmail.com>
2009-05-29 18:48:23 -0700

    echo: check the return code from write
    ...instead of catching it.
    There is no need to catch it as I don't use it later; I just want to
    know there was no error (maybe I should check that all the characters
    were printed...)
    
    [does this count as being suckless? simplifying by 4 lines?]
    
    Signed-off-by: Daniel Cordero <theappleman@gmail.com>

--- echo.c
+++ echo.c
@@ -12,14 +12,10 @@
 
 int main(int argc, char **argv)
 {
-	int error;
 	while (--argc > 0 && ++argv) {
-		error = write(1, *argv, strlen(*argv));
-		if (error == -1)
+		if (write(1, *argv, strlen(*argv)) == -1)
 			return 1;
-
-		error = write(1, argc > 1 ? " " : "\n", 1);
-		if (error == -1)
+		if (write(1, argc > 1 ? " " : "\n", 1) == -1)
 			return 1;
 	}
 	return 0;
