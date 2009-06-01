e6dd12e954c4bf7e9b5fee74f5dd648929298b2f
Daniel Cordero <theappleman@gmail.com>
2009-05-31 21:51:45 -0700

    echo-trivial: remove unnecessary braces
    Signed-off-by: Daniel Cordero <theappleman@gmail.com>

--- echo.c
+++ echo.c
@@ -10,10 +10,9 @@
 
 int main(int argc, char **argv)
 {
-	while (--argc > 0 && *++argv) {
+	while (--argc > 0 && *++argv)
 		if (write(1, *argv, strlen(*argv)) == -1 ||
 			write(1, argc > 1 ? " " : "\n", 1) == -1)
 			return 1;
-	}
 	return 0;
 }
