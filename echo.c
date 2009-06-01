11ed59f52db9d2609f63fdff938adc0fb9b17575
Daniel Cordero <theappleman@gmail.com>
2009-05-31 21:45:11 -0700

    echo: use short-circuit to compress code
    It makes the code slightly harder to read, but it's only slightly.
    
    Signed-off-by: Daniel Cordero <theappleman@gmail.com>

--- echo.c
+++ echo.c
@@ -11,9 +11,8 @@
 int main(int argc, char **argv)
 {
 	while (--argc > 0 && *++argv) {
-		if (write(1, *argv, strlen(*argv)) == -1)
-			return 1;
-		if (write(1, argc > 1 ? " " : "\n", 1) == -1)
+		if (write(1, *argv, strlen(*argv)) == -1 ||
+			write(1, argc > 1 ? " " : "\n", 1) == -1)
 			return 1;
 	}
 	return 0;
